924c896adef1b5ed3b7a406f9a83cbbb156e1276
TulioliAles <desmecad@hotmail.com>
2020-08-03 21:21:29 -0300

    Initial Commit - Criado o projeto Car Sales System

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'car-sales';
+  title = 'Bem vindo ao Car Sales System';
 }
