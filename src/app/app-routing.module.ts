6a0ff5c14f27fc015324428d577ebdaf4c249f87
TulioliAles <desmecad@hotmail.com>
2020-09-15 22:16:04 -0300

    Sixth commit - Implementado Lazy loader para o dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,14 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
 
-const routes: Routes = [{ path: '', component: LoginComponent}];
+const routes: Routes = [{ path: '', component: LoginComponent},
+{
+  path: 'dashboard',
+  loadChildren: () => import('./views/dashboard/dashboard.component').then(
+  (m) => m.DashboardComponent
+    ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { RequestLogin } from 'src/app/resources/models/RequestLogin';
 import { LoginService } from 'src/app/resources/services/login.service';
 import { AlertService } from 'src/app/resources/services/alert.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,9 @@ export class LoginComponent implements OnInit {
 
   public requestLogin: RequestLogin;
 
-  constructor(private loginService: LoginService, private alertService: AlertService) { }
+  constructor(private loginService: LoginService, 
+    private alertService: AlertService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.requestLogin = new RequestLogin();
@@ -21,7 +24,7 @@ export class LoginComponent implements OnInit {
   public doLogin() : void {
     this.loginService.doLogin(this.requestLogin).subscribe(
       (data) => {
-        this.alertService.info("Funcionalidade ainda não implementada!")
+        this.router.navigate(['dashboard']);
       },
       (httperror) => {
         this.alertService.error(httperror.error.message);
